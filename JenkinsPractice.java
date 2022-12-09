7d90e33acc34d5e778a2e164f282c91e8890f8c5
KIRUTHIKA-1515 <120093840+KIRUTHIKA-1515@users.noreply.github.com>
2022-12-09 16:26:04 +0530

    Update JenkinsPractice.java

--- JenkinsPractice.java
+++ JenkinsPractice.java
@@ -4,8 +4,7 @@ System.out.println("KIRUTHIKA");
 System.out.println("VEERAKUMAR");
 System.out.println("MAHALAKSHMI");
 System.out.println("LATHA");
-Sytem.out.println("KIRISHNASAMY");
-Sytem.out.println("JANAKI");
+
 		
 	}	
 
