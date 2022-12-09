9e916590e19b1d0afaf612a3b3e1d7f574b4a19c
KIRUTHIKA-1515 <120093840+KIRUTHIKA-1515@users.noreply.github.com>
2022-12-09 15:41:20 +0530

    Add files via upload

---
+++ JenkinsPractice.java
@@ -0,0 +1,10 @@
+public class JenkinsPractice {
+	public static void main(String[] args) {
+System.out.println("KIRUTHIKA");
+System.out.println("VEERAKUMAR");
+System.out.println("MAHALAKSHMI");
+System.out.println("LATHA");
+		
+	}	
+
+}
